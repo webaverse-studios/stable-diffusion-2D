40edce7632c75f2ba4a59cb1f209835adeae67a5
Arpan Tripathi <tripathiarpan20@gmail.com>
2023-01-25 12:46:21 +0000

    Added out img dims option

--- predict.py
+++ predict.py
@@ -57,13 +57,17 @@ class Predictor(BasePredictor):
         cut_inner_tol:int = Input(description="Inner tolerance in `cutv2` strongest component PNG masking ", default = 7),
         cut_outer_tol:int = Input(description="Outer tolerance in `cutv2` strongest component PNG masking ", default = 35),
         cut_radius:int = Input(description="Radius in `cutv2` strongest component PNG masking ", default = 70),
-        sd_seed:int = Input(description="Seed for SD generations for getting deterministic outputs", default = 1024)
+        sd_seed:int = Input(description="Seed for SD generations for getting deterministic outputs", default = 1024),
+        width:int = Input(description="Width for returning output image", default = None),
+        height:int = Input(description="Height for returning output image", default = None)
     ) -> Any:
         """Run a single prediction on the model"""
         try:
             global pipe_asset, pipe_tile
 
             init_img = load_image_generalised(input)
+
+            orig_img_dims = init_img.shape
 
             images = None
             if req_type == 'asset':
@@ -90,6 +94,17 @@ class Predictor(BasePredictor):
 
 
             external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+
+            if height is None or width is None:
+                if orig_img_dims is None:
+                    height = 512
+                    width = 512
+                else:
+                    height = orig_img_dims[0]
+                    width = orig_img_dims[1]
+
+            for idx in len(images):
+                images[idx] = images[idx].resize((height,width))
 
             images_ = []
 
